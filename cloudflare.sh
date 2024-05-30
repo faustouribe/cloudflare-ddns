3be2978545f8bb5563581a51135d2f0758c30580
faustouribe <faustouribe@gmail.com>
2024-05-29 21:12:21 -0500

    Create cloudflare.sh

---
+++ cloudflare.sh
@@ -0,0 +1,9 @@
+#!/bin/bash -x
+source local
+docker run \
+  --name=cloudflare-ddns \
+  -e API_KEY=$CFAPIKEY \
+  -e ZONE=$CFDOMAIN \
+  -e SUBDOMAIN=$CFSUBDOMAIN \
+  --restart unless-stopped \
+oznu/cloudflare-ddns
